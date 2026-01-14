c7a43a7a9f0b5cf3c80e337152496c9469e0c1e8
pdradx <pdradx@gmail.com>
2026-01-14 15:45:49 +0600

    My startup vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
+vim.g.autoformat = false
+
+local opt = vim.opt
+opt.shiftwidth = 4
+
+-- opt.timeoutlen = 1000
+-- opt.ttimeoutlen = 0
+
+vim.filetype.add({
+  extension = {
+    tpp = "cpp",
+  },
+})
+
+-- vim.g.root_spec = { "cwd" }
